2681964a0e0db1b7675c300257597a542a995401
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-04-15 18:02:33 -0300

    ajuste imagem home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,13 @@
 
 import { ButtonWhats } from "@/components/buttonWhats"
-import produto from "../assets/produto3.jpeg"
+import produto from "../assets/produto3.png"
 
 export function Home(){
     return(
         <div className=" w-full  bg-fitbg">
             <section className="flex flex-col sm:items-center gap-4 pb-4 relative">
                 <img src={produto} alt="" className=" sm:rounded-xl sm:w-[440px] w-full"/>
-                <div className="absolute flex flex-col top-4 right-8 text-zinc-950 font-semibold font-H">
-                    <div className="flex flex-col items-end">
-                        <h1 className="text-5xl font-bold mb-6">Tudo em um</h1>
-                        <h2 className="text-4xl">Proteína</h2>
-                        <h2 className="text-4xl">Creatina</h2>
-                        <h2 className="text-4xl">Fibras</h2>
-                    </div>
-                    
-                </div>
+                
                 <div className="flex flex-col items-center gap-4 px-8">
                     <p className="text-slate-100 text-center text-sm font-Ps">Nosso mix de proteínas foi feito para você que não tem tempo para uma refeição balanceada devido a correria do dia a dia!</p>
                     <ButtonWhats  text="Garanta já o seu!" />
