526b9cd532e187f5d344535b4a3b0c2f160f0e80
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-04-16 10:35:18 -0300

    add video

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,14 @@
 
 import { ButtonWhats } from "@/components/buttonWhats"
 import produto from "../assets/produto3.png"
+import video from "../assets/video/video.mp4"
 
 export function Home(){
     return(
         <div className=" w-full  bg-fitbg">
             <section className="flex flex-col sm:items-center gap-4 pb-4 relative">
-                <img src={produto} alt="" className=" sm:rounded-xl sm:w-[440px] w-full"/>
-                
+                <img src={produto} alt="" className=" sm:rounded-xl sm:w-[440px] hidden"/>
+                <video autoPlay controls loop muted className="sm:w-[400px]"><source src={video} type="video/mp4" /></video>
                 <div className="flex flex-col items-center gap-4 px-8">
                     <p className="text-slate-100 text-center text-sm font-Ps">Nosso mix de proteínas foi feito para você que não tem tempo para uma refeição balanceada devido a correria do dia a dia!</p>
                     <ButtonWhats  text="Garanta já o seu!" />
