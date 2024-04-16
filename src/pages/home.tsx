84b12a88a6206374f24bd084d356f2749bea9e3b
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-04-16 11:26:51 -0300

    ajuste video

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,7 @@ export function Home(){
         <div className=" w-full  bg-fitbg">
             <section className="flex flex-col sm:items-center gap-4 pb-4 relative">
                 <img src={produto} alt="" className=" sm:rounded-xl sm:w-[440px] hidden"/>
-                <video autoPlay controls loop muted className="sm:w-[400px]"><source src={video} type="video/mp4" /></video>
+                <video autoPlay loop muted playsInline className="sm:w-[400px] w-full"><source src={video} type="video/mp4" /></video>
                 <div className="flex flex-col items-center gap-4 px-8">
                     <p className="text-slate-100 text-center text-sm font-Ps">Nosso mix de proteínas foi feito para você que não tem tempo para uma refeição balanceada devido a correria do dia a dia!</p>
                     <ButtonWhats  text="Garanta já o seu!" />
