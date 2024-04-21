ee3c47d9e0f090679243d341f568673dad579a46
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-04-21 16:11:07 -0300

    ajuste carrossel

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -11,8 +11,6 @@ import Autoplay from "embla-carousel-autoplay"
 import produto from "../assets/produto1.jpeg"
 import produto2 from "../assets/produto2.jpeg"
 import produto3 from "../assets/produto6.jpeg"
-import produto4 from "../assets/produto.jpeg"
-import produto5 from "../assets/produto5.jpeg"
 import { ButtonWhats } from "@/components/buttonWhats"
 
 
@@ -58,26 +56,6 @@ export function Product(){
                             <img src={produto3} alt="Produto"  />
                         </div>
                     </CarouselItem>
-                    {/* <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-                        <div className="relative">
-                            <div className="absolute w-full h-24 px-8 pt-3 bg-black/40 bottom-0 font-helvetica-1 text-white">
-                                <p>Pacote Basic(5 unidades): R$ 79,00</p>
-                                <p>Pacote Standard(10 unidades): R$ 139,00</p>
-                                <p>Pacote Premium(20 unidades): R$ 275,00</p>
-                            </div>
-                            <img src={produto4} alt="Produto"  />
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-                        <div className="relative">
-                            <div className="absolute w-full h-24 px-8 pt-3 bg-black/40 bottom-0 font-helvetica-1 text-white">
-                                <p>Pacote Basic(5 unidades): R$ 79,00</p>
-                                <p>Pacote Standard(10 unidades): R$ 139,00</p>
-                                <p>Pacote Premium(20 unidades): R$ 275,00</p>
-                            </div>
-                            <img src={produto5} alt="Produto"  />
-                        </div>
-                    </CarouselItem> */}
                 </CarouselContent>
                 <CarouselPrevious className="hidden sm:flex"/>
                 <CarouselNext  className="hidden sm:flex" />
